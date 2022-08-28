960917a11be5c1b95e7e5a19e328b3d00b9a376e
Julian Rico <ja.rico@uniandes.edu.co>
2022-08-28 11:02:58 -0500

    modelo empleado

--- proyecto3/src/main/java/com/example/proyecto3/Modelo/Empleado.java
+++ proyecto3/src/main/java/com/example/proyecto3/Modelo/Empleado.java
@@ -1,5 +1,50 @@
 package com.example.proyecto3.Modelo;
 
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
+/* 
+id tipo long @id 
+email tipo string @unique
+profile tipo profile
+role tipo Enum_RoleName
+enterprice tipo Enterprice
+transactions tipo transaction[]
+updateAt tipo Date
+createdAt tipo Date
+*/
+
+@Entity
+@Table(name = "empleado")
+
 public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "email", length = 100, unique = true)
+    private String email;
+
+    @Column(name = "profile", length = 50)
+    private Profile profile;
+
+    @Column(name = "role", length = 50)
+    private Enum_roleName role;
+
+    @Column(name = "enterprice", length = 50)
+    private Empresa enterprice;
+
+    @Column(name = "transactions")
+    private List<Transaccion> transactions;
+
+    @Column(name = "createdAt")
+    private Date createdAt;
+
+    @Column(name = "updatedAt")
+    private Date updateAt;
+
     
 }
