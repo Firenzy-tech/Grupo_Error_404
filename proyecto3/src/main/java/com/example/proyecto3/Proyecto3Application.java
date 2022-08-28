2733d06f8be342409773900588d640dfa19234e1
Mimos07 <mimos1107@gmail.com>
2022-08-28 15:42:11 -0500

    Ajustes variables

--- proyecto3/src/main/java/com/example/proyecto3/Proyecto3Application.java
+++ proyecto3/src/main/java/com/example/proyecto3/Proyecto3Application.java
@@ -1,13 +1,24 @@
 package com.example.proyecto3;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import java.sql.Date;
 
-@SpringBootApplication
+import com.example.proyecto3.Modelo.Transaccion;
+
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+//@SpringBootApplication
 public class Proyecto3Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Proyecto3Application.class, args);
+		//SpringApplication.run(Proyecto3Application.class, args);
+		
+		
+		Transaccion mTransaccion = new Transaccion((long) 152356, "Ejemplo de concepto", 322565986);
+
+		System.out.println(mTransaccion.getConcept());
+	
+	
 	}
 
 }
