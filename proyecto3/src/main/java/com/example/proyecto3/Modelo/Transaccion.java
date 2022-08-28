ca2c1bb6311bfd21beb4e5d8071682aed9f91d0d
udeamonica <moniverod@gmail.com>
2022-08-28 11:27:51 -0500

    Adjunto mi declaracion de variables en la clase transaccion

--- proyecto3/src/main/java/com/example/proyecto3/Modelo/Transaccion.java
+++ proyecto3/src/main/java/com/example/proyecto3/Modelo/Transaccion.java
@@ -1,5 +1,38 @@
 package com.example.proyecto3.Modelo;
 
-public class Transaccion {
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "transaccion")
+
+public class Transaccion{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "concept")
+    private String concept;
+
+    @Column(name = "amount")
+    private float amount;
+
+    @Column(name = "user")
+    private User user;
+
+    @Column(name = "enterprise")
+    private Enterprise enterprise;
+
+    @Column(name = "createdAt")
+    private Date createdAt;
+
+    @Column(name = "updatedAt")
+    private Date updateAt;
+
+   
     
 }
