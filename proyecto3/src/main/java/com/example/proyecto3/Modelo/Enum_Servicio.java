d4a88fc1f0d6943d0c1b997c3a98d06080d3842c
Juan <escogil18@gmail.com>
2022-09-05 19:32:12 -0500

    Terce avance

---
+++ proyecto3/src/main/java/com/example/proyecto3/Modelo/Enum_Servicio.java
@@ -0,0 +1,13 @@
+package com.example.proyecto3.Modelo;
+
+public enum Enum_Servicio {
+
+    VACUNA,
+    PELUQUERO,
+    CORTE,
+    ENFERMERIA,
+    BAÑO
+
+
+
+}
--- proyecto3/src/main/java/com/example/proyecto3/Modelo/Servicios.java
+++ proyecto3/src/main/java/com/example/proyecto3/Modelo/Servicios.java
@@ -1,12 +1,13 @@
 package com.example.proyecto3.Modelo;
 
+import java.sql.Date;
 
 import javax.persistence.*;
+
 @Entity
 @Table(name = "Servicios")
 
 public class Servicios {
-    
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -20,7 +21,74 @@ public class Servicios {
     @Column(name = "IdEmpleado", length = 50)
     private Empleado empleado;
 
+    @Column(name = "ClaseServicio", length = 50)
+    private Enum_Servicio claseServicio;
 
+    @Column(name = "Precio")
+    private float precio;
+
+    @Column(name = "createdAt")
+    private Date createdAt;
+
+
+    public Servicios(Long id, Mascota mascota, Empleado empleado, Enum_Servicio claseServicio, float precio, Date createdAt) {
+        this.id = id;
+        this.mascota = mascota;
+        this.empleado = empleado;
+        this.claseServicio = claseServicio;
+        this.precio = precio;
+        this.createdAt = createdAt;
+    }
+
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Mascota getMascota() {
+        return this.mascota;
+    }
+
+    public void setMascota(Mascota mascota) {
+        this.mascota = mascota;
+    }
+
+    public Empleado getEmpleado() {
+        return this.empleado;
+    }
+
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
+    }
+
+    public Enum_Servicio getClaseServicio() {
+        return this.claseServicio;
+    }
+
+    public void setClaseServicio(Enum_Servicio claseServicio) {
+        this.claseServicio = claseServicio;
+    }
+
+    public float getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(float precio) {
+        this.precio = precio;
+    }
+
+    public Date getCreatedAt() {
+        return this.createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
 
 
 }
