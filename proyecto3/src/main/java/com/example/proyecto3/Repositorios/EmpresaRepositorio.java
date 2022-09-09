ab877c5dc3ff5978a847643b9674ad6a451f1a9d
Cristian Lopez <cristian__host@hotmail.com>
2022-09-09 15:15:15 -0500

    Se agrega los controladores de la clase empresa

---
+++ proyecto3/src/main/java/com/example/proyecto3/Repositorios/EmpresaRepositorio.java
@@ -0,0 +1,20 @@
+package com.example.proyecto3.Repositorios;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.example.proyecto3.Modelo.Empresa;
+
+@Repository
+public interface EmpresaRepositorio extends JpaRepository<Empresa, Long> {
+    
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM EMPRESA e WHERE e.id = ?1")
+    Empresa deleteEmpresa(Long id);
+}
+
