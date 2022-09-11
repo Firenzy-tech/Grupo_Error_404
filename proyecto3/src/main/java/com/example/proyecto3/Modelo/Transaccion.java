9e7cd5c1e4fcca596aed72e7430134b64b8d12b6
Miguel Montes <103539534+Mimos07@users.noreply.github.com>
2022-09-10 20:52:41 -0500

    Update Transaccion.java
    Actualización modelo transacción.

--- proyecto3/src/main/java/com/example/proyecto3/Modelo/Transaccion.java
+++ proyecto3/src/main/java/com/example/proyecto3/Modelo/Transaccion.java
@@ -1,9 +1,9 @@
 package com.example.proyecto3.Modelo;
 
-import java.sql.Date;
-import java.util.List;
+import java.util.Date;
 
 import javax.persistence.*;
+
 
 @Entity
 @Table(name = "Transaccion")
@@ -13,41 +13,37 @@ public class Transaccion {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private long id;
 
-    @Column(name = "concept", length = 100)
     private String concept;
-
-    @Column(name = "amount", length = 300)
     private float amount;
 
-    @Column(name = "users")
-    private List<Empleado> user;
+    @ManyToOne
+    @JoinColumn(name="idEmpleado")
+    private Empleado empleado;
 
-    @Column(name = "enterprice", length = 50)
-    private Empresa enterprice;
-    
-    @Column(name = "createdAt")
+    @ManyToOne
+    @JoinColumn(name = "idEmpresa")
+    private Empresa empresa;
+
     private Date createdAt;
-
-    @Column(name = "updatedAt")
     private Date updateAt;
 
+    public Transaccion(){}
 
-    public Transaccion(long id, String concept, float amount, List<Empleado> user, Empresa enterprice, Date createdAt, Date updateAt) {
+    public Transaccion(long id, String concept, float amount, Empleado empleado, Empresa empresa, Date createdAt,
+            Date updateAt) {
         this.id = id;
         this.concept = concept;
         this.amount = amount;
-        this.user = user;
-        this.enterprice = enterprice;
+        this.empleado = empleado;
+        this.empresa = empresa;
         this.createdAt = createdAt;
         this.updateAt = updateAt;
     }
 
-
     public long getId() {
-        return this.id;
+        return id;
     }
 
     public void setId(long id) {
@@ -55,7 +51,7 @@ public class Transaccion {
     }
 
     public String getConcept() {
-        return this.concept;
+        return concept;
     }
 
     public void setConcept(String concept) {
@@ -63,31 +59,31 @@ public class Transaccion {
     }
 
     public float getAmount() {
-        return this.amount;
+        return amount;
     }
 
     public void setAmount(float amount) {
         this.amount = amount;
     }
 
-    public List<Empleado> getUser() {
-        return this.user;
+    public Empleado getEmpleado() {
+        return empleado;
     }
 
-    public void setUser(List<Empleado> user) {
-        this.user = user;
+    public void setEmpleado(Empleado empleado) {
+        this.empleado = empleado;
     }
 
-    public Empresa getEnterprice() {
-        return this.enterprice;
+    public Empresa getEmpresa() {
+        return empresa;
     }
 
-    public void setEnterprice(Empresa enterprice) {
-        this.enterprice = enterprice;
+    public void setEmpresa(Empresa empresa) {
+        this.empresa = empresa;
     }
 
     public Date getCreatedAt() {
-        return this.createdAt;
+        return createdAt;
     }
 
     public void setCreatedAt(Date createdAt) {
@@ -95,15 +91,12 @@ public class Transaccion {
     }
 
     public Date getUpdateAt() {
-        return this.updateAt;
+        return updateAt;
     }
 
     public void setUpdateAt(Date updateAt) {
         this.updateAt = updateAt;
     }
-
-
-
 
     
 }
