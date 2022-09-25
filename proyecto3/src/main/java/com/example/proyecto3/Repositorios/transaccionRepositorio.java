d663b09f661a6f95fde4646e1bba96a93bc7b853
Cristian Lopez <cristian__host@hotmail.com>
2022-09-24 20:18:15 -0500

    v2

--- proyecto3/src/main/java/com/example/proyecto3/Repositorios/transaccionRepositorio.java
+++ proyecto3/src/main/java/com/example/proyecto3/Repositorios/transaccionRepositorio.java
@@ -1,11 +1,9 @@
 package com.example.proyecto3.Repositorios;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
 
 import com.example.proyecto3.Modelo.Transaccion;
 
-@Service
-public interface TransaccionRepositorio extends JpaRepository <Transaccion, Long> {
+public interface TransaccionRepositorio extends JpaRepository<Transaccion, Long>{
     
 }
